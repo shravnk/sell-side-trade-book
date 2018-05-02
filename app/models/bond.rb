40b58163b1a2017d9f58d622fa0edec4e139fe24
shravnk <brendan.ode4@gmail.com>
2018-05-02 19:10:23 -0400

    First draft transacts#index complete

--- app/models/bond.rb
+++ app/models/bond.rb
@@ -3,4 +3,13 @@ class Bond < ApplicationRecord
 	has_many :clients, through: :transacts
 	has_many :traders, through: :transacts
 	has_many :salespeople, through: :transacts
+
+	def print_description
+		"#{self.ticker} #{self.coupon.to_s}% #{self.maturity.year}"
+	end
+
+	def add_to_coupon
+		self.coupon + 1
+	end
+	
 end
--- app/models/transact.rb
+++ app/models/transact.rb
@@ -3,4 +3,12 @@ class Transact < ApplicationRecord
 	belongs_to :client
 	belongs_to :trader
 	has_and_belongs_to_many :salespeople
+
+	def trade_type
+		if self.we_buy
+			"Buy"
+		else
+			"Sell"
+		end
+	end
 end
