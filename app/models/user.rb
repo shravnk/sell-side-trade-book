25fc9d6d8055a6c0c2f492f3f25763f9385d0e88
shravnk <brendan.ode4@gmail.com>
2018-05-17 16:49:20 -0400

    remove error when user adds additional info from github

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,13 @@
 class User < ApplicationRecord
 	has_secure_password
 	
+	validates_presence_of :name, :username, :user_type
 	validates :username, uniqueness: true
 
 	def self.find_or_create_by_omniauth(auth_hash)
 	  username = auth_hash['info']['username']
       	where(:username => username).first_or_create do |user|
-	        user.password = auth_hash['info']['password']
 	        user.name = auth_hash['info']['name']
-	        user.user_type = auth_hash['info']['user_type']
 	        user.uid = auth_hash['uid']
 	  	end
 	end
