e5ea9eb7983885f5d8cfaeb9620a2061ab0a732f
shravnk <brendan.ode4@gmail.com>
2018-05-31 12:08:00 -0400

    refactored next transact jquery, added last trade alert

--- app/assets/javascripts/transacts.js
+++ app/assets/javascripts/transacts.js
@@ -1,69 +1,96 @@
-Handlebars.registerHelper('checkType', function(trade_type, button_type) {
-    if (trade_type == button_type) {
-        return "checked";
-      } else {
-        return ""
-      }
-})
+$(document).ready(function() {
+  setCheckTypeHelper()
+  addNextTransactListener()
+  addDuplicateTransactListener()
+  });
 
-$(".js-next-transact").on("click", function() {
+function addNextTransactListener() {
+    $(".js-next-transact").on("click", function() {
     $("#duplicate").empty()
      var nextId = parseInt($(".js-next-transact").attr("data-id"));
+     if (nextId) {
      $.get("/transacts/" + nextId + ".json", function(data) {
-        var sp_names = []
-        var salespeople = data.salespeople
-        for (i in salespeople) {
-          sp_names.push(salespeople[i]["name"])
-        }
+        setTransactAttributes(data)
+        setTransactLinks(data)
+    })
+    } else {alert("This is the most recent trade.")}
+    })
+}
+
+function setCheckTypeHelper() {
+    Handlebars.registerHelper('checkType', function(trade_type, button_type) {
+        if (trade_type == button_type) {
+            return "checked";
+          } else {
+            return ""
+          }
+    })
+}
+
+function createSalespersonNamesArray(salespeople) {
+    var sp_names = []
+    for (i in salespeople) {
+      sp_names.push(salespeople[i]["name"])
+    }
+    return sp_names
+}
+
+function setTransactAttributes(data) {
         $("#description").text(data["bond"]["description"])
         $("#price").text(data["price"])
         $("#type").text(data["trade_type"])
         $("#size").text(data["size"])
         $("#trader").text(data["trader_name"])
         $("#client").text(data["client_name"])
-        $("#salespeople").text(sp_names.join(', '))
+
         $("#trade_time").text(data["f_trade_time"])
         $("#updated_at").text(data["f_updated_at"])
         $("#created_at").text(data["f_created_at"])
+
+        sp_names = createSalespersonNamesArray(data.salespeople)
+        $("#salespeople").text(sp_names.join(', '))
+}
+
+function setTransactLinks(data) {
         $("#edit").attr("href", `/transacts/${data["id"]}/edit`)
         $("#cancel").attr("href", `/transacts/${data["id"]}/cancel`)
         $("#confirm").attr("href", `/transacts/${data["id"]}/confirm`)
 
         $(".js-next-transact").attr("data-id", data["next_id"])
         $(".js-new-transact").attr("data-id", data["id"])
+}
+
+function addDuplicateTransactListener() {
+    $(".js-new-transact").on("click", function() {
+    var currentId = parseInt($(".js-new-transact").attr("data-id"));
+    $.get("/transacts/" + currentId + ".json").done(function(data) {
+        var bond_id = data["bond"]["id"]
+        var size = data["size"]
+        var price = data["price"]
+        var trade_type = data["trade_type"]
+        var trader_id = data["trader"]["id"] 
+        var client_id = data["client"]["id"]
+
+        var salespeople_ids = []
+        var salespeople = data.salespeople
+        for (i in salespeople) {
+          salespeople_ids.push(salespeople[i]["id"])
+        }
+      
+        var now = new Date()
+        var trade_time = now.toISOString().substring(0, now.toISOString().length-1)
+
+        var tradeDetails = {size, trade_type, price, trade_time}
+
+        var transactFormTemplate = document.getElementById("duplicate-transact-template").innerHTML
+        var template = Handlebars.compile(transactFormTemplate)
+        document.getElementById("duplicate").innerHTML = template(tradeDetails)
+
+        $("#transact_bond_id").val(bond_id)
+        $("#transact_trader_id").val(trader_id)
+        $("#transact_client_id").val(client_id)
+        $("#transact_salesperson_ids").val(salespeople_ids)
       })
-})
-
-
-$(".js-new-transact").on("click", function() {
-var currentId = parseInt($(".js-new-transact").attr("data-id"));
-$.get("/transacts/" + currentId + ".json").done(function(data) {
-    var bond_id = data["bond"]["id"]
-    var size = data["size"]
-    var price = data["price"]
-    var trade_type = data["trade_type"]
-    var trader_id = data["trader"]["id"] 
-    var client_id = data["client"]["id"]
-
-    var salespeople_ids = []
-    var salespeople = data.salespeople
-    for (i in salespeople) {
-      salespeople_ids.push(salespeople[i]["id"])
-    }
-  
-    var now = new Date()
-    var trade_time = now.toISOString().substring(0, now.toISOString().length-1)
-
-    var tradeDetails = {size, trade_type, price, trade_time}
-
-    var transactFormTemplate = document.getElementById("duplicate-transact-template").innerHTML
-    var template = Handlebars.compile(transactFormTemplate)
-    document.getElementById("duplicate").innerHTML = template(tradeDetails)
-
-    $("#transact_bond_id").val(bond_id)
-    $("#transact_trader_id").val(trader_id)
-    $("#transact_client_id").val(client_id)
-    $("#transact_salesperson_ids").val(salespeople_ids)
-  })
-})
+    })
+}
 
