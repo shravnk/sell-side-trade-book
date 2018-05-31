f459d4b844f901b79d01d4a48c471b409d42ee24
shravnk <brendan.ode4@gmail.com>
2018-05-30 21:47:18 -0400

    converted bond's transacts index to handlebars template

--- app/assets/javascripts/bonds.js
+++ app/assets/javascripts/bonds.js
@@ -1,5 +1,8 @@
 $( () => {
+  var displayTradesTemplate = document.getElementById("display-trades-template").innerHTML
+  var template = Handlebars.compile(displayTradesTemplate)
   var requestId = 0
+  
   $(".js-bond-trades").mouseenter(function() {
     var that = this
     $(this).css("background-color", "#d8e7ff")
@@ -8,18 +11,8 @@ $( () => {
         var thisRequestId = requestId
         $.get("/bonds/" + bondId + ".json").done(function(data) {
           transacts = data
-          current = document.querySelectorAll( ":hover" );
-          var curBondId= [].slice.call(current).pop().getAttribute("data-id")
           if (thisRequestId == requestId) {
-            for(let transact of transacts) {
-              $("#js-display-trades-header").show()
-              $("#js-display-trades").append(`<tr>
-                <td>${transact.trade_type}</td>
-                <td>${transact.size}</td>
-                <td>${transact.price}</td>
-                <td>${transact.trader_name}</td>
-                <td>${transact.client_name}</td></tr>`)
-              };
+              document.getElementById("js-display-trades-column").innerHTML = template(transacts)
             }
           });
       }, 100);
